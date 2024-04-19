591e0b1d5a3ae8bc61fbce971d55dd74bb3d8f4b
YingYing Zhang <hi@yingyingzhang.com>
2024-04-19 12:18:11 -0400

    added token

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "tfe" {
+  version  = "~> 0.53.0"
+  token = "0sA4h4baEYu4oQ.atlasv1.Iz2uXqyE5NuI1f47c1EgUpjvryAtGAOzF83EA9vZeHrTdaEHQgXCHdmYM1zzrTC4L18"
+}
+
 resource "tfe_team" "test" {
   name         = "test team"
   organization = "TFE-provider-playground"
