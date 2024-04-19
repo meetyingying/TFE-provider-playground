0d5f62b7b8190f79f249dff878d7f88c2d2f5c4d
YingYing Zhang <hi@yingyingzhang.com>
2024-04-19 14:47:52 -0400

    addd token in the config file

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-provider "tfe" {}
+provider "tfe" {
+  token = "8qTxC1y4K2Qzzw.atlasv1.P8chi283ycIYiKaiWkELlXiRSebXTaiOyRfdddgYVmpXyrQAYd21P2uIxrg2AOSS3gU"
+}
 
-resource "tfe_team" "test" {
-  name         = "test team"
+resource "tfe_team" "example_team" {
+  name         = "example-team"
   organization = "TFE-provider-playground"
 }
