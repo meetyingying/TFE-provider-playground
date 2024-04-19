80e9925fcf7641388ec16c9dba7894bf9c85101c
YingYing Zhang <hi@yingyingzhang.com>
2024-04-19 11:39:28 -0400

    created new org

--- main.tf
+++ main.tf
@@ -7,4 +7,15 @@ terraform {
       name = "TFE-provider-playground"
     }
   }
+
+  required_providers {
+    tfe = {
+      version = "~> 0.53.0"
+    }
+  }
+}
+
+resource "tfe_organization" "a-module-producer" {
+  name  = "TFE-provider-configured-org-YY"
+  email = "yingying@hashicorp.com"
 }
