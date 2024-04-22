032db882a7f126da4c2237fd4d733d491fcb38a3
YingYing Zhang <hi@yingyingzhang.com>
2024-04-22 14:50:36 -0400

    changed team and group name to provider-

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 // Configure TFE provider
 provider "tfe" {}
 
-resource "tfe_team" "example_tf_team" {
-  name         = "example-tf-team"
+resource "tfe_team" "provider_tf_team" {
+  name         = "provider-tf-team"
   organization = "YingYing-sandbox"
 }
 
 // Configure HCP provider
 provider "hcp" {}
 
-resource "hcp_group" "example_hcp_team" {
-  display_name = "example-hcp-group"
+resource "hcp_group" "provider_hcp_team" {
+  display_name = "provider-hcp-group"
   description  = "HCP group created with HCP provider"
 }
