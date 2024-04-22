1d2041adce5a9e6a6be809a17ed284ee8bdb59be
YingYing Zhang <hi@yingyingzhang.com>
2024-04-22 11:52:18 -0400

    would it work without quote?

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 // Configure TFE provider
 provider "tfe" {}
 
-resource "tfe_team" "example_tf_team" {
+resource tfe_team "example_tf_team" {
   name         = "example-tf-team"
   organization = "TFE-provider-playground"
 }
@@ -9,7 +9,7 @@ resource "tfe_team" "example_tf_team" {
 // Configure HCP provider
 provider "hcp" {}
 
-resource "hcp_group" "example_hcp_team" {
+resource hcp_group "example_hcp_team" {
   display_name = "example-hcp-group"
   description  = "HCP group created with HCP provider"
 }
