d37802ef7b3327937a844dc54dbee73170529de8
YingYing Zhang <hi@yingyingzhang.com>
2024-04-22 11:47:35 -0400

    create group with HCP provider

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
+// Configure TFE provider
 provider "tfe" {}
 
-resource "tfe_team" "example_team" {
-  name         = "example-team"
+resource "tfe_team" "example_tf_team" {
+  name         = "example-tf-team"
   organization = "TFE-provider-playground"
+}
+
+// Configure HCP provider
+provider "hcp" {}
+
+resource "hcp_group" "example_hcp_team" {
+  display_name = "example-hcp-group"
+  description  = "HCP group created with HCP provider"
 }
