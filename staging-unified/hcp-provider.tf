fd13ab06cc21dd7379b56f8c7cecc2323d5b3fd4
YingYing Zhang <hi@yingyingzhang.com>
2024-04-23 15:09:07 -0400

    can hcp provider invite users?

---
+++ staging-unified/hcp-provider.tf
@@ -0,0 +1,44 @@
+# configure HCP provider
+# specify using "default project"
+provider "hcp" {
+  project_id = "31b0608f-4755-4b81-a239-ae3fe06570be"
+}
+
+# create a new project
+resource "hcp_project" "provider_hcp_project" {
+  name        = "provider-hcp-project"
+  description = "Project created with HCP provider"
+}
+
+# create service principals in the project
+resource "hcp_service_principal" "provider_service_principal_1" {
+  name = "provider-service-principal-1"
+  parent = hcp_project.provider_hcp_project.resource_name
+}
+resource "hcp_service_principal" "provider_service_principal_2" {
+  name = "provider-service-principal-2"
+  parent = hcp_project.provider_hcp_project.resource_name
+}
+
+# list users for reference
+data "hcp_user_principal" "yingying_1" {
+  email = "yingying+1@hashicorp.com"
+}
+data "hcp_user_principal" "yingying_jan23" {
+  email = "yingying+jan23@hashicorp.com"
+}
+
+# create a new group
+resource "hcp_group" "provider_hcp_group" {
+  display_name = "provider-hcp-group"
+  description  = "HCP group created with HCP provider"
+}
+
+# add the users to the group
+resource "hcp_group_members" "provider_hcp_group_members" {
+  group = hcp_group.provider_hcp_group.resource_name
+  members = [
+    data.hcp_user_principal.yingying_1.user_id,
+    data.hcp_user_principal.yingying_jan23.user_id,
+  ]
+}
--- staging-unified/tfe-provider.tf
+++ staging-unified/tfe-provider.tf
@@ -7,27 +7,3 @@ provider "tfe" {
 data "tfe_organization" "TFC_Unification_Test_Org_2" {
   name = "TFC-Unification-Test-Org-2"
 }
-
-# create a project in the org
-resource "tfe_project" "provider_tf_project" {
-  organization = data.tfe_organization.TFC_Unification_Test_Org_2.name
-  name = "provider-tf-project"
-}
-
-# invite user yingying+standalone to the org
-resource "tfe_organization_membership" "yingying_standalone" {
-  organization  = data.tfe_organization.TFC_Unification_Test_Org_2.name
-  email = "yingying-standalone@hashicorp.com"
-}
-
-# create a team in the org
-resource "tfe_team" "provider_tf_team" {
-  organization = data.tfe_organization.TFC_Unification_Test_Org_2.name
-  name         = "provider-tf-team"
-}
-
-# add user yingying+standalone to the team
-resource "tfe_team_organization_member" "yingying_standalone" {
-  team_id = tfe_team.provider_tf_team.id
-  organization_membership_id = tfe_organization_membership.yingying_standalone.id
-}
