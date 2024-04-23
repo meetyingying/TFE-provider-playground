34d1990583f6f6d5aa16235e380137335b6fe0c8
YingYing Zhang <hi@yingyingzhang.com>
2024-04-23 12:33:29 -0400

    swapped service principals with users, and add to the group

--- staging-standalone/hcp-provider.tf
+++ staging-standalone/hcp-provider.tf
@@ -1,15 +1,6 @@
 # configure HCP provider
 provider "hcp" {
-}
-
-# add the organization for reference
-data "hcp_organization" "yingying_org" {
-}
-
-# create a new group
-resource "hcp_group" "provider_hcp_group" {
-  display_name = "provider-hcp-group"
-  description  = "HCP group created with HCP provider"
+  project_id = "4fe900f2-2b94-42fe-8ebb-8b0410d39840"
 }
 
 # create a new project
@@ -18,21 +9,35 @@ resource "hcp_project" "provider_hcp_project" {
   description = "Project created with HCP provider"
 }
 
-# create a couple service principals in the project
+# create service principals in the project
 resource "hcp_service_principal" "provider_service_principal_1" {
   name = "provider-service-principal-1"
-  parent = data.hcp_organization.yingying_org.resource_name
+  parent = hcp_project.provider_hcp_project.resource_name
 }
 resource "hcp_service_principal" "provider_service_principal_2" {
   name = "provider-service-principal-2"
-  parent = data.hcp_organization.yingying_org.resource_name
+  parent = hcp_project.provider_hcp_project.resource_name
 }
 
-# add the service principals to the group
+# add users for reference
+data "hcp_user_principal" "yingying_1" {
+  email = "yingying+1@hashicorp.com"
+}
+data "hcp_user_principal" "yingying_jan23" {
+  email = "yingying+jan23@hashicorp.com"
+}
+
+# create a new group
+resource "hcp_group" "provider_hcp_group" {
+  display_name = "provider-hcp-group"
+  description  = "HCP group created with HCP provider"
+}
+
+# add the users to the group
 resource "hcp_group_members" "provider_hcp_group_members" {
   group = hcp_group.provider_hcp_group.resource_name
   members = [
-    hcp_service_principal.provider_service_principal_1.resource_id,
-    hcp_service_principal.provider_service_principal_2.resource_id,
+    hcp_user_principal.yingying_1.id,
+    hcp_user_principal.yingying_jan23.id,
   ]
 }
