9e3dcf88cda86fd3ece05dc8cd0d30faff72391e
YingYing Zhang <hi@yingyingzhang.com>
2024-04-23 12:05:53 -0400

    added parents for the service principals

--- staging-standalone/hcp-provider.tf
+++ staging-standalone/hcp-provider.tf
@@ -7,12 +7,20 @@ resource "hcp_group" "provider_hcp_group" {
   description  = "HCP group created with HCP provider"
 }
 
-# create a couple service principals
+# create a new project
+resource "hcp_project" "provider_hcp_project" {
+  name        = "provider-hcp-project"
+  description = "Project created with HCP provider"
+}
+
+# create a couple service principals in the project
 resource "hcp_service_principal" "provider_service_principal_1" {
   name = "provider-service-principal-1"
+  parent = hcp_project.provider_hcp_project.resource_name
 }
 resource "hcp_service_principal" "provider_service_principal_2" {
   name = "provider-service-principal-2"
+  parent = hcp_project.provider_hcp_project.resource_name
 }
 
 # add members to the group
