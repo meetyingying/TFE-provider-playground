c91f20b961b016fdcd4ce9eccd25184043e9e171
YingYing Zhang <hi@yingyingzhang.com>
2024-05-14 15:59:56 -0400

    remove required provider block

--- staging-standalone/terraform.tf
+++ staging-standalone/terraform.tf
@@ -7,17 +7,4 @@ terraform {
       name = "TFE-provider-playground"
     }
   }
-
-  required_providers {
-    tfe = {
-      source = "hashicorp/tfe"
-      version = "~> 0.53.0"
-    }
-    hcp = {
-      source  = "hashicorp/hcp"
-      version = "~> 0.86.0"
-    }
-  }
-  
-  required_version = "~> 1.2"
 }
