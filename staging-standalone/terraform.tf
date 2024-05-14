117a6f8c65e072761a85157a1610d8b6ad4c7ea3
YingYing Zhang <hi@yingyingzhang.com>
2024-05-14 16:02:21 -0400

    add required provider block back

--- staging-standalone/terraform.tf
+++ staging-standalone/terraform.tf
@@ -7,4 +7,17 @@ terraform {
       name = "TFE-provider-playground"
     }
   }
+
+required_providers {
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "~> 0.53.0"
+    }
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.86.0"
+    }
+  }
+  
+  required_version = "~> 1.2"
 }
