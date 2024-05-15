46dee63236b5ad746b8a7185c61db18ecf33e8e8
YingYing Zhang <hi@yingyingzhang.com>
2024-05-15 18:10:13 -0400

    assign admin role of a group to a project

--- staging-unified/tfe-provider.tf
+++ staging-unified/tfe-provider.tf
@@ -3,24 +3,7 @@ provider "tfe" {
   hostname = "app.staging.terraform.io"
 }
 
-# reference a unified organization
-data "tfe_organization" "TFC_Unification_Test_Org_2" {
-  name = hcp_organization.TFC_Unification_Test_Org_2.name
-}
-
-# reference a unified group
-data "tfe_team" "provider_hcp_group" {
-  name         = hcp_project.provider_hcp_project.name
-  organization = hcp_organization.TFC_Unification_Test_Org_2.name
-}
-
-# reference a unified project
-data "tfe_project" "provider_hcp_project" {
-  name         = hcp_project.provider_hcp_project.name
-  organization = tfe_organization.TFC_Unification_Test_Org_2.name
-}
-
-# reference a unified project
+# assign a unified group admin role to a unified project
 resource "tfe_team_project_access" "provider_hcp_group_project_access" {
   access       = "admin"
   team_id      = hcp_group.provider_hcp_group.resource_id
