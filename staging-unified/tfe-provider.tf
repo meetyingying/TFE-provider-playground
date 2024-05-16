50c88371aa72af75aec782287119a44c806032e5
YingYing Zhang <hi@yingyingzhang.com>
2024-05-16 11:25:42 -0400

    added depends on hcp project

--- staging-unified/tfe-provider.tf
+++ staging-unified/tfe-provider.tf
@@ -8,4 +8,5 @@ resource "tfe_team_project_access" "provider_hcp_group_project_access" {
   access       = "admin"
   team_id      = hcp_group.provider_hcp_group.resource_id
   project_id   = hcp_project.provider_hcp_project.resource_id
+  depends_on = [ hcp_project.provider_hcp_project ]
 }
